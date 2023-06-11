fbd8ece0e40e8ed8417a489544e3d0c050ea28e6
Mlungisi Vilakazi <mlungisiv@hotmail.com>
2023-06-11 15:41:24 +0200

    add basic retrieve and display functionality

---
+++ employees/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='images')
+    designation = models.CharField(max_length=100)
+    email_address = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=12, blank=True)
+    create_at = models.DateTimeField(auto_now_add=True) #auto now add - first time creation datetime will be added
+    updated_at = models.DateTimeField(auto_now=True) # will add date time every time field changes
+
+    def __str__(self):
+        return self.first_name
+
+
